88c6a6618c88aac91fa938097fee9b6d596d69b8
Jack Potter <jpot0003@student.monash.edu>
2023-03-10 10:31:23 +1100

    Updated skills section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About'
+import Skills from './components/Skills'
+import Scrollbar from './components/Scrollbar'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+import { HashRouter } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <div>
+          <Navbar />
+          {/* <Scrollbar /> */}
+          <Hero />
+          {/* <About /> */}
+          <Skills />
+          <Projects />
+          <Contact />
+      </div>
+    </HashRouter>
   );
 }
 
--- src/skillData.js
+++ src/skillData.js
@@ -1,7 +1,7 @@
 export default[
     {
         title:"Web Development",
-        description:"Well-versed in languages such as HTML, CSS and JavaScript, as well as frameworks such as React.js.",
+        description:"Expertise in developing user-friendly front-end applications for the web, using HTML, CSS and React.js.",
         image:"./test.png"
     },
     {
@@ -10,8 +10,8 @@ export default[
         image:"./database.png"
     },
     {
-        title:"Microsoft Azure",
-        description:"Certified in the use of Microsoft Azure cloud solutions.",
-        image:"./computer.png"
+        title:"Mobile Applications",
+        description:"Developing full-stack native applications on both iOS and Android devices.",
+        image:"./phone-icon.png"
     }
 ]
